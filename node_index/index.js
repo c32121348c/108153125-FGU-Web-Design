863018cdce1c966edfd0990d4bf6d1a7c1f44abe
c32121348c <c32121348c@gmail.com>
2020-12-09 23:29:47 +0800

    2020/12/04 homework

--- node_index/index.js
+++ node_index/index.js
@@ -1,48 +1,54 @@
 var http = require('http');
 
-let staticFile = (response, fname) => {
-	let fs = require('fs');
+const route_table = {
+  '/': '../OAOhtml/index.html',
+  '/QAQ': '../OAOhtml/styles.css',
+  '/OAO': '../OAOhtml/index.js',
+};
 
-  	fs.readFile(fname, (err, data) => {
-    	if(err){
-      		console.log(`檔案讀取錯誤: ${err}`);
-    	}
-    	else{
-			response.writeHead(200, {'Content-Type': 'text/html'});
-      		response.write(data);
-      		response.end();
-    	} // if
-  	});
+const mime_type = {
+  '.css': 'text/css',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+};
+
+let staticFile = (response, fname, mime) => {
+  let fs = require('fs');
+
+  fs.readFile(fname, (err, data) => {
+    if (err) {
+      console.log(`檔案讀取錯誤: ${err}`);
+    }
+    else {
+      response.writeHead(200, {
+//        'Content-Type': 'text/html',
+        'Content-Type': mime,
+      });
+
+      response.write(data);
+      response.end();
+    } // if
+  });
 } // staticFile()
 
 http.createServer((request, response) => {
-	request.on('data', (chunk) => {console.log(`data chunk: ${chunk}`);}).on('end', () => {
-		let url = require('url');
-    	let pathname = url.parse(request.url).pathname;
+  request.on('data', (chunk) => {
+    console.log(`data chunk: ${chunk}`);
+  }).on('end', () => {
+    let path = require('path');
+    let url = require('url');
+    let pathname = url.parse(request.url).pathname;
+    let fname = route_table[pathname];
 
-		switch (pathname) {
-			case '/':
-				staticFile(response, '../OAOhtml/index.html');
-				break;
+    if (fname) {
+      staticFile(
+        response, fname, mime_type[path.extname(fname)]
+      );
+    }
 
-			case '/styles.css':
-				staticFile(response, '../OAOhtml/styles.css');
-				break;
-
-			case '/index.js':
-				staticFile(response, '../OAOhtml/index.js');
-				break;
-				
-			default:
-				break;
-    	} // switch
-
-		console.log(`Request for ${pathname}`);
-	
-	});
+    console.log(`Request for ${pathname}`);
+  });
 }).listen(8080);
 
-// log message to Console log message to Console
+// log message to Console
 console.log('Server running at http://127.0.0.1:8080/');
-
-// index.js.
